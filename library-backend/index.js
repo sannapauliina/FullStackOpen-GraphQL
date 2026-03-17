b40ff15fee5f9dcb8b09b0140b6aa5de5245a4d4
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-03-17 15:38:22 +0200

    Tehtävä 8.5: laajennettu allBooks-kyselyä genre-parametrilla ja tuettu author+genre -yhdistelmät

--- library-backend/index.js
+++ library-backend/index.js
@@ -95,7 +95,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -106,11 +106,19 @@ const resolvers = {
     authorCount: () => authors.length,
 
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books;
+      let filteredBooks = books;
+
+      if (args.author) {
+        filteredBooks = filteredBooks.filter((b) => b.author === args.author);
       }
 
-      return books.filter((b) => b.author === args.author);
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((b) =>
+          b.genres.includes(args.genre),
+        );
+      }
+
+      return filteredBooks;
     },
 
     allAuthors: () => authors,
